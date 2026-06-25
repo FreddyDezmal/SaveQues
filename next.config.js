47384687d2fd2fa07020975d0bff92954f5299fb
FreddyDezmal <mohaufrederick@gmail.com>
2026-06-25 07:01:43 +0200

    fix: add PostHog CDN domains to CSP

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const nextConfig = {
           key: "Content-Security-Policy",
           value: [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' https://app.posthog.com",
+            "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://app.posthog.com https://us.i.posthog.com https://us-assets.i.posthog.com https://sentry.io https://*.sentry.io",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
             "img-src 'self' data: blob:",
